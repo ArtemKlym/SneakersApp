c720a139b430206b15e891d81359414c15754984
Artem <artemklymenko892@gmail.com>
2024-07-29 13:06:30 +0300

    Created SignUpScreen and SignUpViewModel

---
+++ app/src/main/java/com/artemklymenko/sneakersapp/pages/sign_up/SignUpViewModel.kt
@@ -0,0 +1,9 @@
+package com.artemklymenko.sneakersapp.pages.sign_up
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(): ViewModel(){
+}
