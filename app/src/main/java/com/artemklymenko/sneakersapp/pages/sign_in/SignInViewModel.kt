d9bd882dca697f75d9d094fa0cc046f5b7116a8a
Artem <artemklymenko892@gmail.com>
2024-07-29 13:06:47 +0300

    Created SignInScreen and SignInViewModel

---
+++ app/src/main/java/com/artemklymenko/sneakersapp/pages/sign_in/SignInViewModel.kt
@@ -0,0 +1,9 @@
+package com.artemklymenko.sneakersapp.pages.sign_in
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SignInViewModel @Inject constructor(): ViewModel(){
+}
