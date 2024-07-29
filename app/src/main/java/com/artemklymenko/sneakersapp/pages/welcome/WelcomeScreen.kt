04e3f84b14a0b2e9cfa46ac2e0cfccec64586c06
Artem <artemklymenko892@gmail.com>
2024-07-29 13:05:33 +0300

    Created WelcomeScreen and WelcomeViewModel

---
+++ app/src/main/java/com/artemklymenko/sneakersapp/pages/welcome/WelcomeScreen.kt
@@ -0,0 +1,60 @@
+package com.artemklymenko.sneakersapp.pages.welcome
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import com.artemklymenko.sneakersapp.ui.theme.Purple40
+
+@Composable
+fun WelcomeScreen(
+    viewModel: WelcomeViewModel,
+    onNavigationNext: () -> Unit
+) {
+    Scaffold(
+        topBar = { TopBar() },
+        content = {
+            WelcomeScreenContent(
+                modifier = Modifier.padding(it),
+                onNavigationNext = onNavigationNext
+            )
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun TopBar() {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Welcome",
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(Purple40)
+    )
+}
+
+@Composable
+private fun WelcomeScreenContent(
+    modifier: Modifier,
+    onNavigationNext: () -> Unit
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = "Welcome screen",
+            modifier = modifier.clickable {
+                onNavigationNext.invoke()
+            }
+        )
+    }
+}
