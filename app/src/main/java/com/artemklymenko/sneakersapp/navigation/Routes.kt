fab63d4dbb3af2144c94f9cf3035d28462db444d
Artem <artemklymenko892@gmail.com>
2024-08-01 12:15:26 +0300

    Added more routes

--- app/src/main/java/com/artemklymenko/sneakersapp/navigation/Routes.kt
+++ app/src/main/java/com/artemklymenko/sneakersapp/navigation/Routes.kt
@@ -1,9 +1,37 @@
 package com.artemklymenko.sneakersapp.navigation
 
 sealed class Routes(val route: String) {
-    object Splash: Routes("splash")
-    object Welcome: Routes("welcome")
-    object SignIn: Routes("sign_in")
-    object SignUp: Routes("sign_up")
-    object Main: Routes("main")
+    /**
+     * Entry point
+     */
+    data object Splash: Routes("splash")
+
+    /**
+     * Welcome screen for users
+     */
+    data object Welcome: Routes("welcome")
+
+    /**
+     * Authentication
+     */
+    data object SignIn: Routes("sign_in")
+    data object SignUp: Routes("sign_up")
+
+    /**
+     * Main screen: first page
+     */
+    data object Main: Routes("main")
+    data object Feed: Routes("main/feed")
+    data object Search : Routes("feed/search")
+    data object Favourites: Routes("main/favourites")
+    data object Cart: Routes("main/cart")
+    data object Profile: Routes("main/profile")
+
+    /**
+     * Nested Graphs
+     */
+    data object FeedGraph : Routes("feed_graph")
+    data object FavouritesGraph : Routes("favourites_graph")
+    data object CartGraph : Routes("cart_graph")
+    data object ProfileGraph : Routes("profile_graph")
 }
