d1c3b6543bca32b535b3f76395252e26824eff9c
Artem <artemklymenko892@gmail.com>
2024-08-06 18:10:52 +0300

    Created ConfirmScreenScreen

---
+++ app/src/main/java/com/artemklymenko/sneakersapp/pages/confirm/ConfirmViewModel.kt
@@ -0,0 +1,9 @@
+package com.artemklymenko.sneakersapp.pages.confirm
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ConfirmViewModel @Inject constructor(): ViewModel() {
+}
