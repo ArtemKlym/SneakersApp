15fc03075a49580d87c23c83a5e82fd1757e62a2
Artem <artemklymenko892@gmail.com>
2024-08-06 18:11:17 +0300

    Created BillingDetailsScreen

---
+++ app/src/main/java/com/artemklymenko/sneakersapp/pages/main/pages/profile/billing/BillingDetailsViewModel.kt
@@ -0,0 +1,9 @@
+package com.artemklymenko.sneakersapp.pages.main.pages.profile.billing
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class BillingDetailsViewModel @Inject constructor(): ViewModel() {
+}
