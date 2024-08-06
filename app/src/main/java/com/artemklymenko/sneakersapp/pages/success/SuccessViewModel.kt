ac379bcb480a8f37638c671511bb5163137df213
Artem <artemklymenko892@gmail.com>
2024-08-06 18:08:59 +0300

    Created SuccessScreen

---
+++ app/src/main/java/com/artemklymenko/sneakersapp/pages/success/SuccessViewModel.kt
@@ -0,0 +1,9 @@
+package com.artemklymenko.sneakersapp.pages.success
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SuccessViewModel @Inject constructor(): ViewModel() {
+}
