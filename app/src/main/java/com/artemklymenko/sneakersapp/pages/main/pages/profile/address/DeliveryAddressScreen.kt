56a8057f7749f21c133feec5ad2cbc8251ef4902
Artem <artemklymenko892@gmail.com>
2024-08-06 18:10:34 +0300

    Created DeliveryAddressScreen

---
+++ app/src/main/java/com/artemklymenko/sneakersapp/pages/main/pages/profile/address/DeliveryAddressScreen.kt
@@ -0,0 +1,36 @@
+package com.artemklymenko.sneakersapp.pages.main.pages.profile.address
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import com.artemklymenko.sneakersapp.core.components.TopBar
+
+@Composable
+fun DeliveryAddressScreen(
+    viewModel: DeliveryAddressViewModel,
+    onBackClick: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopBar(
+                title = "Delivery Address",
+                onBackClick = { onBackClick.invoke() }
+            )
+        },
+        content = {
+            DeliveryAddressScreenContent(Modifier.padding(it))
+        }
+    )
+}
+
+@Composable
+private fun DeliveryAddressScreenContent(modifier: Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Delivery Address", fontSize = 22.sp, color = Color.Red)
+    }
+}
