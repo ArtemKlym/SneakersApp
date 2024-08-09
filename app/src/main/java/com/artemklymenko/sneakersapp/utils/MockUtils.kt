154a0fb9c53b0d6993a9a735c1a1a5562385e073
Artem <artemklymenko892@gmail.com>
2024-08-09 19:45:55 +0300

    Added more functions to MockUtils

--- app/src/main/java/com/artemklymenko/sneakersapp/utils/MockUtils.kt
+++ app/src/main/java/com/artemklymenko/sneakersapp/utils/MockUtils.kt
@@ -1,6 +1,7 @@
 package com.artemklymenko.sneakersapp.utils
 
 import com.artemklymenko.sneakersapp.domain.models.Notification
+import com.artemklymenko.sneakersapp.domain.models.Product
 
 object MockUtils {
     fun loadMockNotifications(): List<Notification> {
@@ -55,6 +56,24 @@ object MockUtils {
                 false,
             )
         )
-
     }
-}
+
+        fun loadMockProducts(): List<Product> {
+            return listOf(
+                Product(1, "Product 1", 39.99, "https://as2.ftcdn.net/v2/jpg/02/11/11/15/1000_F_211111574_VLtzH6ORhebXvnJXjlkAkaUuAftnvmJH.jpg", false),
+                Product(2, "Product 2", 39.99, "https://as1.ftcdn.net/v2/jpg/02/60/22/68/1000_F_260226888_aZ3EJOTSE2ly5TlRuseWexnVkK67KqBl.jpg", true),
+                Product(3, "Product 3", 52.85, "https://as1.ftcdn.net/v2/jpg/03/14/89/16/1000_F_314891647_CVY0iUdvxyJ2D6RfP6jNvktTWYdNvy6y.jpg", false),
+                Product(4, "Product 4", 74.42, "https://as1.ftcdn.net/v2/jpg/02/64/98/34/1000_F_264983445_wXVAKMmZe0Q1jJN6R6riVJsajb7fGvx3.jpg", true),
+                Product(5, "Product 5", 95.21, "https://as2.ftcdn.net/v2/jpg/02/11/11/15/1000_F_211111574_VLtzH6ORhebXvnJXjlkAkaUuAftnvmJH.jpg", false),
+                Product(6, "Product 6", 11.11, "https://as1.ftcdn.net/v2/jpg/02/60/22/68/1000_F_260226888_aZ3EJOTSE2ly5TlRuseWexnVkK67KqBl.jpg", true),
+                Product(7, "Product 7", 22.22, "https://as1.ftcdn.net/v2/jpg/02/60/22/68/1000_F_260226888_aZ3EJOTSE2ly5TlRuseWexnVkK67KqBl.jpg", false),
+                Product(8, "Product 8", 33.33, "https://as1.ftcdn.net/v2/jpg/03/14/89/16/1000_F_314891647_CVY0iUdvxyJ2D6RfP6jNvktTWYdNvy6y.jpg", true),
+                Product(9, "Product 9", 44.44, "https://as1.ftcdn.net/v2/jpg/03/14/89/16/1000_F_314891647_CVY0iUdvxyJ2D6RfP6jNvktTWYdNvy6y.jpg", false),
+                Product(10, "Product 10", 55.55, "https://as1.ftcdn.net/v2/jpg/02/64/98/34/1000_F_264983445_wXVAKMmZe0Q1jJN6R6riVJsajb7fGvx3.jpg", true),
+            )
+        }
+
+        fun loadMockSearchCategories(): List<String> {
+            return listOf("All", "Top", "Price", "New")
+        }
+    }
