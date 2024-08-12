0ebb4dc97b9343ecee41e7da13dd7719c65f0706
Artem <artemklymenko892@gmail.com>
2024-08-12 16:57:43 +0300

    Created FeedUiEvent

---
+++ app/src/main/java/com/artemklymenko/sneakersapp/pages/main/pages/feed/FeedUiEvent.kt
@@ -0,0 +1,11 @@
+package com.artemklymenko.sneakersapp.pages.main.pages.feed
+
+import com.artemklymenko.sneakersapp.core.base.common.events.UiEvent
+
+sealed class FeedUiEvent : UiEvent {
+    data object LoadScreenData : FeedUiEvent()
+
+    data class OnProductClick(val productId: Long) : FeedUiEvent()
+
+    data class OnFavouriteClick(val favouriteId: Long) : FeedUiEvent()
+}
