0a1f99cd58413a7e83e282a3f6b856f5fc836c71
Artem <artemklymenko892@gmail.com>
2024-08-29 20:22:51 +0300

    Implemented ProfileScreen

--- app/src/main/java/com/artemklymenko/sneakersapp/pages/main/pages/profile/ProfileScreen.kt
+++ app/src/main/java/com/artemklymenko/sneakersapp/pages/main/pages/profile/ProfileScreen.kt
@@ -1,15 +1,51 @@
 package com.artemklymenko.sneakersapp.pages.main.pages.profile
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.artemklymenko.sneakersapp.core.components.TopBar
+import coil.compose.rememberAsyncImagePainter
+import com.artemklymenko.sneakersapp.R
+import com.artemklymenko.sneakersapp.core.base.BaseContentLayout
+import com.artemklymenko.sneakersapp.core.components.TopBarAsText
 
 @Composable
 fun ProfileScreen(
@@ -17,49 +53,173 @@ fun ProfileScreen(
     onNavigateToSettings: () -> Unit,
     onNavigateToPersonalDetails: () -> Unit,
     onNavigateToDeliveryAddress: () -> Unit,
-    onNavigateToBillingDetails: () -> Unit
+    onNavigateToBillingDetails: () -> Unit,
+    onNavigateToLogOut: () -> Unit,
 ) {
-    Scaffold(
-        topBar = {
-            TopBar(title = "Profile")
-        },
-        content = {
+    LaunchedEffect(key1 = Unit) {
+        viewModel.handleUiEvent(
+            ProfileUiEvent.LoadData
+        )
+    }
+    BaseContentLayout(viewModel = viewModel) { uiState ->
+        uiState?.let {
             ProfileScreenContent(
-                modifier = Modifier.padding(it),
                 onNavigateToSettings = onNavigateToSettings,
                 onNavigateToPersonalDetails = onNavigateToPersonalDetails,
                 onNavigateToDeliveryAddress = onNavigateToDeliveryAddress,
-                onNavigateToBillingDetails = onNavigateToBillingDetails
+                onNavigateToBillingDetails = onNavigateToBillingDetails,
+                onNavigateToLogOut = onNavigateToLogOut,
+                user = it
             )
         }
-    )
+    }
 }
 
 @Composable
 private fun ProfileScreenContent(
-    modifier: Modifier,
     onNavigateToSettings: () -> Unit,
     onNavigateToPersonalDetails: () -> Unit,
     onNavigateToDeliveryAddress: () -> Unit,
-    onNavigateToBillingDetails: () -> Unit
+    onNavigateToBillingDetails: () -> Unit,
+    user: ProfileUiState,
+    onNavigateToLogOut: () -> Unit
 ) {
-    Column(modifier = modifier) {
-        Text(text = "This is profile screen", fontSize = 22.sp, color = Color.Red)
-        Text(
-            text = "To settings",
-            modifier.clickable { onNavigateToSettings.invoke() }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TopBarAsText(title = stringResource(id = R.string.label_profile))
+        Image(
+            painter = rememberAsyncImagePainter(model = user.urlImage),
+            contentScale = ContentScale.Crop,
+            contentDescription = null,
+            modifier = Modifier
+                .size(240.dp)
+                .padding(top = 8.dp)
+                .clip(RoundedCornerShape(24.dp)),
         )
         Text(
-            text = "To personal details",
-            modifier.clickable { onNavigateToPersonalDetails.invoke() }
+            modifier = Modifier.padding(top = 32.dp),
+            text = user.name + " " + user.surname,
+            fontWeight = FontWeight.Bold,
+            fontSize = 32.sp
         )
         Text(
-            text = "To delivery address",
-            modifier.clickable { onNavigateToDeliveryAddress.invoke() }
+            modifier = Modifier.padding(top = 8.dp),
+            text = user.email,
+            fontWeight = FontWeight.Normal,
+            fontSize = 22.sp
         )
-        Text(
-            text = "To billing details",
-            modifier.clickable { onNavigateToBillingDetails.invoke() }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 40.dp)
+                .clip(RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp))
+                .background(
+                    color = MaterialTheme.colorScheme.secondaryContainer,
+                    shape = RectangleShape
+                ),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(24.dp))
+            ProfileItem(
+                icon = Icons.Default.Settings,
+                title = stringResource(R.string.settings),
+                onClick = onNavigateToSettings
+            )
+            ProfileItem(
+                icon = Icons.Filled.Done,
+                title = stringResource(R.string.billing_details),
+                onClick = onNavigateToBillingDetails
+            )
+            ProfileItem(
+                icon = Icons.Default.Home,
+                title = stringResource(R.string.delivery_address_details),
+                onClick = onNavigateToDeliveryAddress
+            )
+            ProfileItem(
+                icon = Icons.Default.Person,
+                title = stringResource(R.string.personal_data_details),
+                onClick = onNavigateToPersonalDetails
+            )
+            ProfileItem(
+                icon = Icons.AutoMirrored.Default.ExitToApp,
+                title = stringResource(R.string.log_out),
+                onClick = onNavigateToLogOut
+            )
+        }
+
+    }
+
+}
+
+@Composable
+private fun ProfileItem(icon: ImageVector, title: String, onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Card(
+                modifier = Modifier.size(32.dp),
+                shape = RoundedCornerShape(16.dp),
+                elevation = CardDefaults.elevatedCardElevation(8.dp),
+                border = BorderStroke(1.dp, Color.Black)
+            ) {
+                Image(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(4.dp),
+                    imageVector = icon,
+                    contentDescription = title,
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Text(
+                modifier = Modifier
+                    .clickable {
+                        onClick.invoke()
+                    },
+                text = title,
+            )
+        }
+        Icon(
+            modifier = Modifier
+                .size(24.dp)
+                .clickable {
+                    onClick.invoke()
+                },
+            imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+            contentDescription = null
         )
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileScreenContentPreview() {
+    ProfileScreenContent(
+        onNavigateToSettings = { /*TODO*/ },
+        onNavigateToPersonalDetails = { /*TODO*/ },
+        onNavigateToDeliveryAddress = { /*TODO*/ },
+        onNavigateToBillingDetails = { /*TODO*/ },
+        user = ProfileUiState(
+            id = 1,
+            name = "Jack",
+            surname = "London",
+            email = "jack.london@gmail.com",
+            urlImage = "https://hips.hearstapps.com/hmg-prod/images/gettyimages-3346353-copy.jpg"
+        )
+    ) {
+
     }
 }
