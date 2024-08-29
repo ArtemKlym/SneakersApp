6e242eb865186af78d7c2f7a853e3b6c012b7a12
Artem <artemklymenko892@gmail.com>
2024-08-29 20:22:37 +0300

    Updated ProfileViewModel

--- app/src/main/java/com/artemklymenko/sneakersapp/pages/main/pages/profile/ProfileViewModel.kt
+++ app/src/main/java/com/artemklymenko/sneakersapp/pages/main/pages/profile/ProfileViewModel.kt
@@ -1,9 +1,40 @@
 package com.artemklymenko.sneakersapp.pages.main.pages.profile
 
-import androidx.lifecycle.ViewModel
+import com.artemklymenko.sneakersapp.core.base.BaseViewModel
+import com.artemklymenko.sneakersapp.core.base.common.events.CallBack
+import com.artemklymenko.sneakersapp.core.base.common.events.Dialog
+import com.artemklymenko.sneakersapp.core.base.common.events.Progress
+import com.artemklymenko.sneakersapp.core.base.common.events.UiEvent
+import com.artemklymenko.sneakersapp.utils.MockUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
 import javax.inject.Inject
 
 @HiltViewModel
-class ProfileViewModel @Inject constructor(): ViewModel() {
+class ProfileViewModel @Inject constructor()
+    : BaseViewModel<ProfileUiState, Progress, Dialog, CallBack>() {
+    override fun handleUiEvent(uiEvent: UiEvent) {
+        when(uiEvent){
+            is ProfileUiEvent.LoadData -> {
+                loadData()
+            }
+        }
+    }
+
+    private fun loadData() {
+        launch {
+            val user = async { MockUtils.loadMockUser() }
+            val response = user.await()
+
+            updateState { currentState ->
+                currentState.value = ProfileUiState(
+                    id = response.id,
+                    name = response.name,
+                    surname = response.surname,
+                    email = response.email,
+                    urlImage = response.urlImage
+                )
+            }
+        }
+    }
 }
