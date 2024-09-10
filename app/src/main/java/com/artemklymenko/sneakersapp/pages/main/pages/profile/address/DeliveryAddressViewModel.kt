826c383e893b1da3ad626631a13c4ce16013a3e5
Artem <artemklymenko892@gmail.com>
2024-09-10 07:53:39 +0300

    Updated DeliveryAddressViewModel

--- app/src/main/java/com/artemklymenko/sneakersapp/pages/main/pages/profile/address/DeliveryAddressViewModel.kt
+++ app/src/main/java/com/artemklymenko/sneakersapp/pages/main/pages/profile/address/DeliveryAddressViewModel.kt
@@ -1,9 +1,34 @@
 package com.artemklymenko.sneakersapp.pages.main.pages.profile.address
 
-import androidx.lifecycle.ViewModel
+import com.artemklymenko.sneakersapp.core.base.BaseViewModel
+import com.artemklymenko.sneakersapp.core.base.common.events.CallBack
+import com.artemklymenko.sneakersapp.core.base.common.events.Dialog
+import com.artemklymenko.sneakersapp.core.base.common.events.Progress
+import com.artemklymenko.sneakersapp.core.base.common.events.UiEvent
+import com.artemklymenko.sneakersapp.utils.MockUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
 import javax.inject.Inject
 
 @HiltViewModel
-class DeliveryAddressViewModel @Inject constructor(): ViewModel() {
+class DeliveryAddressViewModel @Inject constructor() :
+    BaseViewModel<DeliveryAddressUiState, Progress, Dialog, CallBack>() {
+    override fun handleUiEvent(uiEvent: UiEvent) {
+        if (uiEvent == DeliveryAddressUiEvent.LoadData) {
+            loadData()
+        }
+    }
+
+    private fun loadData() {
+        launch {
+            val addresses = async { MockUtils.loadMockAddresses() }
+            val response = addresses.await()
+
+            updateState { currentState ->
+                currentState.value = DeliveryAddressUiState(
+                    addresses = response
+                )
+            }
+        }
+    }
 }
