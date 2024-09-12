c7468235461b0285ca97442173a81ac1eeaaef5c
Artem <artemklymenko892@gmail.com>
2024-09-12 19:20:05 +0300

    Updated BillingDetailsViewModel

--- app/src/main/java/com/artemklymenko/sneakersapp/pages/main/pages/profile/billing/BillingDetailsViewModel.kt
+++ app/src/main/java/com/artemklymenko/sneakersapp/pages/main/pages/profile/billing/BillingDetailsViewModel.kt
@@ -1,9 +1,33 @@
 package com.artemklymenko.sneakersapp.pages.main.pages.profile.billing
 
-import androidx.lifecycle.ViewModel
+import com.artemklymenko.sneakersapp.core.base.BaseViewModel
+import com.artemklymenko.sneakersapp.core.base.common.events.CallBack
+import com.artemklymenko.sneakersapp.core.base.common.events.Dialog
+import com.artemklymenko.sneakersapp.core.base.common.events.Progress
+import com.artemklymenko.sneakersapp.core.base.common.events.UiEvent
+import com.artemklymenko.sneakersapp.utils.MockUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
 import javax.inject.Inject
 
 @HiltViewModel
-class BillingDetailsViewModel @Inject constructor(): ViewModel() {
+class BillingDetailsViewModel @Inject constructor(): BaseViewModel<BillingDetailsUiState, Progress, Dialog, CallBack>() {
+    override fun handleUiEvent(uiEvent: UiEvent) {
+        if(uiEvent == BillingDetailsUiEvent.LoadBillingDetailsData){
+            loadData()
+        }
+    }
+
+    private fun loadData() {
+        launch {
+            val paymentMethods = async { MockUtils.loadMockPaymentMethods() }
+            val response = paymentMethods.await()
+
+            updateState { mutableState ->
+                mutableState.value = BillingDetailsUiState(
+                    paymentMethods = response
+                )
+            }
+        }
+    }
 }
