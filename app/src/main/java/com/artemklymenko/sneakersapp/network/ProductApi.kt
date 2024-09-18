6f3838aba653abd72663913f7996c1d5e450cb10
Artem <artemklymenko892@gmail.com>
2024-09-18 20:26:55 +0300

    Created ProductApi interface

---
+++ app/src/main/java/com/artemklymenko/sneakersapp/network/ProductApi.kt
@@ -0,0 +1,14 @@
+package com.artemklymenko.sneakersapp.network
+
+import com.artemklymenko.sneakersapp.domain.models.network.Product
+import com.artemklymenko.sneakersapp.domain.models.network.Products
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ProductApi {
+    @GET("products/category/mens-shoes/product/{id}")
+    suspend fun fetchShoes(@Path("id") id: Int): Product
+
+    @GET("products/category/mens-shoes")
+    suspend fun getAllShoes(): Products
+}
