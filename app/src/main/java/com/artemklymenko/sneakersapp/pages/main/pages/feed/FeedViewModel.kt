5370b34cefbeaa8a61c473af803b6bb72fb8e828
Artem <artemklymenko892@gmail.com>
2024-09-18 20:29:52 +0300

    Updated FeedScreen to load data using Retrofit

--- app/src/main/java/com/artemklymenko/sneakersapp/pages/main/pages/feed/FeedViewModel.kt
+++ app/src/main/java/com/artemklymenko/sneakersapp/pages/main/pages/feed/FeedViewModel.kt
@@ -1,17 +1,22 @@
 package com.artemklymenko.sneakersapp.pages.main.pages.feed
 
+import android.util.Log
+import androidx.lifecycle.viewModelScope
 import com.artemklymenko.sneakersapp.core.base.BaseViewModel
 import com.artemklymenko.sneakersapp.core.base.common.events.CallBack
 import com.artemklymenko.sneakersapp.core.base.common.events.Dialog
 import com.artemklymenko.sneakersapp.core.base.common.events.Progress
 import com.artemklymenko.sneakersapp.core.base.common.events.UiEvent
+import com.artemklymenko.sneakersapp.network.repository.ProductRepository
 import com.artemklymenko.sneakersapp.utils.MockUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class FeedViewModel @Inject constructor(): BaseViewModel<FeedUiState, Progress, Dialog, CallBack>(){
+class FeedViewModel @Inject constructor(
+    private val productRepository: ProductRepository
+): BaseViewModel<FeedUiState, Progress, Dialog, CallBack>(){
     override fun handleUiEvent(uiEvent: UiEvent) {
         when(uiEvent){
             is FeedUiEvent.LoadScreenData -> {
@@ -53,18 +58,34 @@ class FeedViewModel @Inject constructor(): BaseViewModel<FeedUiState, Progress, 
     }
 
     private fun loadScreenData() {
-        launch {
-            val products = async { MockUtils.loadMockProducts() }
-            val searchCategories = async { MockUtils.loadMockSearchCategories()}
-            val response = products.await() to searchCategories.await()
+        viewModelScope.launch {
+            try {
+                val response = productRepository.getAllShoes()
 
-            updateState { currentState ->
-                currentState.value = FeedUiState(
-                    products = response.first,
-                    filteredProducts = response.first,
-                    searchCategories = response.second
-                )
+                updateState { currentState ->
+                    currentState.value = FeedUiState(
+                        products = response.products,
+                        filteredProducts = response.products,
+                        searchCategories = MockUtils.loadMockSearchCategories()
+                    )
+                }
+            }catch (e: Exception){
+                Log.e("FeedViewModel", "Failed to load screen data: ${e.message}")
             }
         }
+//        Load local products
+//        launch {
+//            val products = async { MockUtils.loadMockProducts() }
+//            val searchCategories = async { MockUtils.loadMockSearchCategories()}
+//            val response = products.await() to searchCategories.await()
+//
+//            updateState { currentState ->
+//                currentState.value = FeedUiState(
+//                    products = response.first,
+//                    filteredProducts = response.first,
+//                    searchCategories = response.second
+//                )
+//            }
+//        }
     }
 }
