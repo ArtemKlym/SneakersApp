ae9455d1fadf38824e41b45b87ef7b9e148f3aad
Artem <artemklymenko892@gmail.com>
2024-09-20 20:19:05 +0300

    Updated ProductViewModel for network integration

--- app/src/main/java/com/artemklymenko/sneakersapp/pages/product/ProductViewModel.kt
+++ app/src/main/java/com/artemklymenko/sneakersapp/pages/product/ProductViewModel.kt
@@ -1,18 +1,20 @@
 package com.artemklymenko.sneakersapp.pages.product
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.artemklymenko.sneakersapp.core.base.BaseViewModel
 import com.artemklymenko.sneakersapp.core.base.common.events.CallBack
 import com.artemklymenko.sneakersapp.core.base.common.events.Dialog
 import com.artemklymenko.sneakersapp.core.base.common.events.Progress
 import com.artemklymenko.sneakersapp.core.base.common.events.UiEvent
-import com.artemklymenko.sneakersapp.utils.MockUtils
+import com.artemklymenko.sneakersapp.network.repository.ProductRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ProductViewModel @Inject constructor(): BaseViewModel<ProductUiState, Progress, Dialog, CallBack>() {
+class ProductViewModel @Inject constructor(
+    private val repository: ProductRepository
+): BaseViewModel<ProductUiState, Progress, Dialog, CallBack>() {
     override fun handleUiEvent(uiEvent: UiEvent) {
         when(uiEvent){
             is ProductUiEvent.LoadData -> {
@@ -27,27 +29,25 @@ class ProductViewModel @Inject constructor(): BaseViewModel<ProductUiState, Prog
         }
     }
 
-    private fun loadData(id: Long) {
-        launch {
-            val product = async { MockUtils.loadMockProductDetails(id) }
-            val isProductInCart = async { MockUtils.isProductInCart(id) }
-            val response = product.await() to isProductInCart.await()
+    private fun loadData(id: Int) {
+         viewModelScope.launch {
+            val response = repository.fetchShoes(id)
 
             updateState { currentState ->
                 currentState.value = ProductUiState(
-                    product = response.first,
-                    isAddedToCart = response.second
+                    product = response,
+                    isAddedToCart = false
                 )
             }
         }
     }
 
     private fun updateFavouriteState() {
-        updateState { mutableState ->
-            mutableState.value?.let { state ->
-                val product = state.product.copy(isFavourite = !state.product.isFavourite)
-                mutableState.value = state.copy(product = product)
-            }
-        }
+//        updateState { mutableState ->
+//            mutableState.value?.let { state ->
+//                val product = state.product.copy(isFavourite = !state.product.isFavourite)
+//                mutableState.value = state.copy(product = product)
+//            }
+//        }
     }
 }
