708e23d7288bc8b931593dd048eb02484892f9a7
Artem <artemklymenko892@gmail.com>
2024-09-20 20:17:35 +0300

    Updated ProductUiState data class fields for network integration

--- app/src/main/java/com/artemklymenko/sneakersapp/pages/product/ProductUiState.kt
+++ app/src/main/java/com/artemklymenko/sneakersapp/pages/product/ProductUiState.kt
@@ -2,8 +2,10 @@ package com.artemklymenko.sneakersapp.pages.product
 
 import com.artemklymenko.sneakersapp.core.base.common.state.UiState
 import com.artemklymenko.sneakersapp.domain.models.local.ProductDetails
+import com.artemklymenko.sneakersapp.domain.models.network.Product
 
 data class ProductUiState(
-    val product: ProductDetails,
+    //val product: ProductDetails, local
+    val product: Product,
     val isAddedToCart: Boolean
 ) : UiState
